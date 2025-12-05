64f5e406f7a7faa89ca283c55c7207007abc1a5b
Julien Zigan <zigan.julien@gmail.com>
2025-12-05 15:29:18 +0100

    [Feature] add minimal reservation controller

--- src/main/java/de/steinuntersteinen/hotel/controller/ReservationController.java
+++ src/main/java/de/steinuntersteinen/hotel/controller/ReservationController.java
@@ -1,10 +1,17 @@
 package de.steinuntersteinen.hotel.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import de.steinuntersteinen.hotel.model.Reservation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/reservations")
 class ReservationController {
 
+    @GetMapping
+    public ResponseEntity<Reservation> getReservation() {
+        return  ResponseEntity.ok( new Reservation("1", null, null, "Returnin a Reservation Object works", "peter@mail.com", 2));
+    }
 }
--- src/main/java/de/steinuntersteinen/hotel/model/Reservation.java
+++ src/main/java/de/steinuntersteinen/hotel/model/Reservation.java
@@ -1,4 +1,100 @@
 package de.steinuntersteinen.hotel.model;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Reservation {
+
+    private String id;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private String guestName;
+    private String guestEmail;
+    private int roomNumber;
+
+    public Reservation(String id,
+                       Date checkInDate,
+                       Date checkOutDate,
+                       String guestName,
+                       String guestEmail,
+                       int roomNumber) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.guestName = guestName;
+        this.guestEmail = guestEmail;
+        this.roomNumber = roomNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getGuestEmail() {
+        return guestEmail;
+    }
+
+    public void setGuestEmail(String guestEmail) {
+        this.guestEmail = guestEmail;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Reservation that = (Reservation) o;
+        return roomNumber == that.roomNumber && Objects.equals(id, that.id) && Objects.equals(checkInDate, that.checkInDate) && Objects.equals(checkOutDate, that.checkOutDate) && Objects.equals(guestName, that.guestName) && Objects.equals(guestEmail, that.guestEmail);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, checkInDate, checkOutDate, guestName, guestEmail, roomNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id='" + id + '\'' +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                ", guestName='" + guestName + '\'' +
+                ", guestEmail='" + guestEmail + '\'' +
+                ", roomNumber=" + roomNumber +
+                '}';
+    }
 }
