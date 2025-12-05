f4c644ab447a2d251c299ed670c9e083c45b92f9
Julien Zigan <zigan.julien@gmail.com>
2025-12-05 17:17:26 +0100

    [Tests] adjust return obj for e2e test

--- src/main/java/de/steinuntersteinen/hotel/controller/ReservationController.java
+++ src/main/java/de/steinuntersteinen/hotel/controller/ReservationController.java
@@ -6,12 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/reservations")
 class ReservationController {
 
     @GetMapping
-    public ResponseEntity<Reservation> getReservation() {
-        return  ResponseEntity.ok( new Reservation("1", null, null, "Returnin a Reservation Object works", "peter@mail.com", 2));
+    public ResponseEntity<List<Reservation>> getReservations() {
+        return  ResponseEntity.ok(
+                Arrays.asList(
+                        new Reservation[] {
+                                new Reservation("1",
+                                        null,
+                                        null,
+                                        "Returnin a Reservation Object works",
+                                        "peter@mail.com",
+                                        2
+                                )}
+                )
+            );
     }
 }
